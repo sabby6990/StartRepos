26c92878a420fe1a8a7210c153ffa1fc708dcbd9
saurabh <saurabh@sabby>
2016-12-03 22:36:55 +0530

    prime numbers some more ways to do including efficient way.

--- JavaKnowledge/src/Algos/PrimeNos.java
+++ JavaKnowledge/src/Algos/PrimeNos.java
@@ -1,7 +1,27 @@
 package Algos;
 
 public class PrimeNos {
-	public static void main(String[] args) {
+	
+	// preventing extra loops
+	private static void efficientWay(){
+		
+		for(int i=2;i<10;i++){
+			int count =0;
+			for(int j=2;j<=i/2;j++){
+				if(i%j==0){
+					count++;
+					break;
+				}
+			}
+			if(count ==0){
+				System.out.print(i+",");
+			}
+		}
+	}
+	
+	// forward looping
+	private static void simpleWay(){
+
 		for (int i = 1; i < 100; i++) {
 			int counter =0;
 			for(int j=1;j<=i;j++){
@@ -18,5 +38,35 @@ public class PrimeNos {
 				System.out.print(i+",");
 			}
 		}
+	
+	}
+	
+	// using reverse looping 
+	private static void otherWay(){
+		for (int i = 1; i < 100; i++) {
+			int counter =0;
+			for(int j=i;j>=1;j--){
+				if(i==1){
+					counter=2;
+					break;
+				}
+				if(i%j==0){
+					counter++;
+				}
+			}
+			if(counter ==2){
+				// divisible by 1 and itself so two !!
+				System.out.print(i+",");
+			}
+		}
+	}
+	
+	public static void main(String[] args) {
+		
+		PrimeNos.efficientWay();
+		System.out.println("\n----------------------");
+		PrimeNos.simpleWay();
+		System.out.println("\n----------------------");
+		PrimeNos.otherWay();
 	}
 }
