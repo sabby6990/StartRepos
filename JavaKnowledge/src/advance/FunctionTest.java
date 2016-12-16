222e729b40c0312209f1b244e485ff1797c2c550
saurabh <andriodsaurabh@gmail.com>
2016-12-16 19:37:55 +0530

    HOF 1

--- JavaKnowledge/src/advance/FunctionTest.java
+++ JavaKnowledge/src/advance/FunctionTest.java
@@ -5,18 +5,41 @@ import java.util.function.Supplier;
 
 public class FunctionTest {
 
-    public static void main(String[] args) {
-    	/*
-    	 * the function returns a string while passing a long as paramter
-    	 * */
-    	
-    	/**
-    	 * TODO check how can you implement a counter variable using the below code where the function
-    	 * will just accept a counter integer value and return a incremented value
-    	 * */
-        Function<Supplier<Long>, String> function = (t) -> "Value is " + t.get();
-        String value = function.apply(() -> 1L);
-        System.out.println(value);
-    }
+	public static void main(String[] args) {
+		/*
+		 * the function returns a string while passing a long as paramter
+		 */
 
+		/**
+		 * TODO check how can you implement a counter variable using the below
+		 * code where the function will just accept a counter integer value and
+		 * return a incremented value
+		 */
+		// Function<Supplier<String>, String> function = (t) -> {
+		// return t.get();
+		// };
+		// String value = function.apply(() -> getCount("test"));
+		// System.out.println(value);
+		// System.out.println(getCount("test"));
+		//
+		// functionTest(() -> getCount("test"));
+		System.out.println(functionTest().apply(() -> "test2"));
+		System.out.println(functionTest().apply(() -> 9));
+	}
+
+	static Long getCount() {
+		return 2L;
+	}
+
+	private static <T> T getCount(T t) {
+		return t;
+	}
+
+	private static <T, R> Function<Supplier<T>, T> functionTest() {
+		Function<Supplier<T>, T> function = (t) -> {
+			return t.get();
+		};
+		return function;
+
+	}
 }
